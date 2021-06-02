a8e3e3e95f1b7edd86d342c974b64acd656d42f0
Akbar Rizki Maulana <akbarezeki@gmail.com>
2021-06-02 12:33:53 +0700

    first commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class landing extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+
+                <h3>This is my new website! However, it's still in development, so please be patient.</h3>
+            </div>
+        )
+    }
+}
